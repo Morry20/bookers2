ee48ab4ffbf109afbfbdcf3521ab46fc7b2703c0
Morry20 <ku.l.taf54@gmail.com>
2021-07-24 05:55:26 +0000

    [finish]bookers2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '20d01b11a7a53319156f7ba5e35b1181ef13939d2b2e6b8015f3c8043273a1175a60ee2a084d7ae743e68b9fe948ea4f3cbb384c2ac7d586372691f2ca9009b6'
